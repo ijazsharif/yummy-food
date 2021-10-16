163b05efd5bd3c84bb1f12be1b42464bba30f456
cybrnode-dev <dev@cybrnode.com>
2021-10-16 15:42:35 +0500

    Refactor: app title changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/theme/colors.dart';
 import 'package:food_delivery/views/screens/home_screen.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,14 +15,16 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key, required this.app}) : super(key: key);
   final FirebaseApp app;
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Yummy Food',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: primaryColor,
+        ),
+        primaryColor: primaryColor,
       ),
       home: HomeScreen(app: app),
     );
