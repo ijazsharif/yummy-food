898741cafb6277f6a1e74530f71fbc81ebebec72
ijazsharif <ijazsharif34@gmail.com>
2021-10-20 23:23:24 +0500

    Refactor: best deals widget extracted

--- lib/views/screens/restaurant_home.dart
+++ lib/views/screens/restaurant_home.dart
@@ -1,20 +1,11 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-
-import 'package:google_fonts/google_fonts.dart';
-
-import 'package:food_delivery/theme/strings.dart';
-import 'package:food_delivery/models/popular_item.dart';
+import 'package:food_delivery/widgets/restaurant_home/best_deals_widget.dart';
+import 'package:food_delivery/widgets/restaurant_home/most_popular_widget.dart';
 import 'package:food_delivery/models/restaurant.dart';
-import 'package:food_delivery/view_model/restaurant_home_vm/restaurant_home_vm.dart';
-import 'package:food_delivery/view_model/restaurant_home_vm/restaurant_home_vm_imp.dart';
 
 class RestaurantHome extends StatelessWidget {
-  RestaurantHome({Key? key, required this.restaurant}) : super(key: key);
+  const RestaurantHome({Key? key, required this.restaurant}) : super(key: key);
   final Restaurant restaurant;
-
-  final RestaurantHomeViewModel _viewModel = RestaurantHomeViewModelImp();
 
   @override
   Widget build(BuildContext context) {
@@ -28,48 +19,11 @@ class RestaurantHome extends StatelessWidget {
           child: Column(
             children: [
               Expanded(
-                child: SizedBox(
-                  child: FutureBuilder(
-                    future: _viewModel.displayMostPopularByRestaurantId(
-                      restaurant.restaurantId!,
-                    ),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator.adaptive(),
-                        );
-                      }
-                      if (snapshot.hasError) {
-                        return Text(snapshot.error.toString());
-                      } else {
-                        var listPopular = snapshot.data as List<PopularItem>;
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              Strings.mostPopularText,
-                              style: GoogleFonts.jetBrainsMono(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.black45,
-                              ),
-                            ),
-                            Text(
-                              listPopular.length.toString(),
-                              style: GoogleFonts.jetBrainsMono(),
-                            ),
-                          ],
-                        );
-                      }
-                    },
-                  ),
-                ),
+                child: MostPopularWidget(restaurant: restaurant),
               ),
               Expanded(
                 flex: 2,
-                child: Container(
-                  color: Colors.orange,
-                ),
+                child: BestDealsWidget(restaurant: restaurant,),
               ),
             ],
           ),
@@ -78,3 +32,5 @@ class RestaurantHome extends StatelessWidget {
     );
   }
 }
+
+
