78a16e2a27cfb574af1020959bce2bae6e4b4c3f
ijazsharif <ijazsharif34@gmail.com>
2021-10-20 23:13:55 +0500

    Feature: menu bg color added

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/widgets.dart';
 const primaryColor = Color(0XFFFF0000);
 const white = Color(0xFFFFFFFF);
 const black = Color(0xFF000000);
+
+const Color menuBgColor = Color(0xFFB71C1C);
