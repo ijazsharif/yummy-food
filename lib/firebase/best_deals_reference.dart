d5139bc1098c638cbd08e902e2da432315887afe
ijazsharif <ijazsharif34@gmail.com>
2021-10-20 23:11:12 +0500

    Feature:best deals refernce

---
+++ lib/firebase/best_deals_reference.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:food_delivery/models/popular_item.dart';
+import 'package:food_delivery/utils/constants.dart';
+
+Future<List<PopularItem>> getBestDealsByRestaurantId(
+    String restaurantId) async {
+  var list = List<PopularItem>.empty(growable: true);
+  var source = await FirebaseDatabase.instance
+      .reference()
+      .child(restaurantRef)
+      .child(restaurantId)
+      .child(bestDealsRef)
+      .once();
+  Map<dynamic, dynamic> values = source.value;
+
+  values.forEach(
+    (key, value) {
+      list.add(
+        PopularItem.fromJson(
+          jsonDecode(jsonEncode(value)),
+        ),
+      );
+    },
+  );
+  return list;
+}
